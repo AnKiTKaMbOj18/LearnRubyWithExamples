8b601fc038a452ca0753ade06f6bc30663ae7d7e
Ankit Kamboj <ankit.kamboj@idc.bedbath.com>
2020-01-19 00:08:00 +0530

    RUBY | getting started | installation & local setup | hello ruby example

---
+++ 0.WhatIsRuby.rb
@@ -0,0 +1,25 @@
+#WHAT_IS_RUBY -
+  # Ruby is a dynamic, open source, object oriented and reflective programming language.
+  # Ruby is considered similar to Perl and Smalltalk programming languages.
+  # It runs on all types of platforms like Windows, Mac OS and all versions of UNIX.
+  # It is fully object oriented programming language.
+  # Everything is an object in Ruby. Each and every code has their properties and actions.
+  # Here properties refer to variables and actions refer to methods.
+  # Ruby is considered to follow the principle of POLA (principle of least astonishment).
+  # It means that the language behaves in such a way to minimize the confusion for experienced users.
+
+#Ruby Installation
+  # for install ruby and setup your system visit - https://rubyinstaller.org/downloads/
+  # As of JAN 18,2020 I have installed (Ruby+Devkit 2.6.X (x64) for windows)
+  # stable version and most gems support available
+
+#Creating hello ruby program
+  # use any text editor use below code and open terminal/command propmt to run
+  # file name is helloruby.rb , helloruby is any name (you can give name what you like) and
+  # .rb is ruby extension i.e which defines its ruby file same like we use .js or .java
+  puts "Hello Ruby";
+  # puts in ruby is same as console.log in javascript and system.out.println in java
+  # we can also use print in ruby like print "Hello Ruby" this also works,but doesn't add new line.
+  #above is helloe ruby program , save this file ,
+  # open command prompt/terminal in working directory and write below command to run
+  # ruby helloruby.rb
