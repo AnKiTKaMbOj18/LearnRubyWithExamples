efa6c0efb95fc9d180b6bee6a50ccb30e2d723f6
Ankit Kamboj <ankit.kamboj@idc.bedbath.com>
2020-01-19 18:52:22 +0530

    RUBY | learning about ruby comments

---
+++ 13.comments.rb
@@ -0,0 +1,24 @@
+#Ruby comments -
+=begin
+  Ruby comments are non executable lines in a program.
+  These lines are ignored by the interpreter hence they don't
+  execute while execution of a program.
+  They are written by a programmer to explain their code
+  so that others who look at the code will understand it in a better way.
+
+  Types of Ruby comments:
+  Single line comment
+  multi line comment
+=end
+
+#Single line comment - this is single line comment , applicable only for single line
+  #line starting with #sign is single line comment in ruby
+
+#Multi line comment -
+=begin
+  The Ruby multi line comment is used to comment multiple lines at a time.
+  They are defined with =begin at the starting and =end at the end of the line.
+=end
+
+
+## Note - this whole file is made of comments some are single line and some multi line
