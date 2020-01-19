de4df079fcfe49a66b9c15b5849733083534c259
Ankit Kamboj <ankit.kamboj@idc.bedbath.com>
2020-01-19 20:21:56 +0530

    RUBY | Learning methods/functions in ruby

---
+++ 15.methods.rb
@@ -0,0 +1,23 @@
+#Ruby Methods
+=begin
+  Ruby methods prevent us from writing the same code in a program again and again.
+  It is a set of expression that returns a value.
+  Ruby methods are similar to the functions in other lnguages.
+  They unite one or more repeatable statements into one single bundle.
+
+#Defining Method
+  To use a method, we need to first define it.
+  Ruby method is defined with the def keyword followed by method name.
+  At the end we need to use end keyword to denote that method has been defined.
+=end
+
+def welcomeMethod()
+  puts "welcome to my tutorials repo, hope docs are worth reading for you guys";
+  puts "please let me know for suggestions , changes or for more information";
+  puts "please follow for further updates or reach out to me at ak.ankitkamboj18@gmail.com"
+end
+
+welcomeMethod()
+
+#Note - These methods are similar to methods in other languages i.e in java/javscript/c
+        #with little syntax changes
